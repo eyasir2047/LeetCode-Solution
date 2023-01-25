e82a390f9348a50f02845b5460098b923616d498
Abrar Eyasir <97697364+eyasir2047@users.noreply.github.com>
2023-01-25 22:42:06 +0600

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    System.out.println(str);
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        int cnt=0;
+        ArrayList<Integer>v=new ArrayList<Integer>();
+     //   ArrayList<String> cars = new ArrayList<String>();
+     
+     int maxfromright=arr[n-1];
+     
+        
+       v.add(arr[n-1]);
+       for(int i=n-2;i>=0;i--){
+           if(arr[i]>=maxfromright){
+               maxfromright=arr[i];
+               v.add(arr[i]);
+           }
+       }
+    
+       
+       ArrayList<Integer> revArrayList = new ArrayList<Integer>();
+        for (int i = v.size() - 1; i >= 0; i--) {
+ 
+            // Append the elements in reverse order
+            revArrayList.add(v.get(i));
+        }
+ 
+        // Return the reversed arraylist
+        return revArrayList;
+        
+        
+        
+    
+    }
+}
