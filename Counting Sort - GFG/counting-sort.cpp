921eed362fbe88cb6d01b0a0696155cadee29685
Abrar Eyasir <97697364+eyasir2047@users.noreply.github.com>
2023-01-28 15:38:39 +0600

    Added solution - LeetHub

--- Counting Sort - GFG/counting-sort.cpp
+++ Counting Sort - GFG/counting-sort.cpp
@@ -15,24 +15,73 @@ class Solution{
     //order using Counting Sort.
     string countSort(string arr){
         // code here
-        int count[26]={0};
         int n=arr.size();
+        int count[26]={0};
         for(int i=0;i<n;i++){
             count[arr[i]-'a']++;
         }
-        //abcde. // 1 1 1 1 1
-        //aaabbddd 3 2 0 3 
-        int index=0;
-        for(int i=0;i<=25;i++){
-            while(count[i]>0){
-                arr[index]= 'a'+i;
-                index++;
-                count[i]--;
-            }
+        
+        //prefix sum
+        
+        for(int i=1;i<26;i++){
+            count[i]=count[i]+count[i-1];
         }
         
-        return arr;
+        char result[n];
         
+        //stable sort
+        
+        for(int i=n-1;i>=0;i--){
+            result[--count[arr[i]-'a']]=arr[i];
+        }
+        
+        string s;
+        for(int i=0;i<n;i++){
+            s+=result[i];
+        }
+        return s;
+        
+        
+/*
+   // int maxi=INT_MIN;
+
+   // for(int i=0;i<n;i++){
+    //    maxi=max(maxi,arr[i]);
+
+ //   }
+ int n=arr.size();
+
+    vector<int>count(26,0);
+
+    for(int i=0;i<n;i++){
+        count[arr[i]]++;
+    }
+
+
+    //prefix sum
+
+    for(int i=1;i<=26;i++){
+        count[i]=count[i]+count[i-1];
+    }
+
+   // vector<string>output(n,0);
+   
+   char output[n];
+
+    for(int i=0;i<n;i++){
+
+        output[count[arr[i]-1]]=arr[i];
+        --count[arr[i]];
+
+    }
+
+    //for(int i=0;i<n;i++){
+      //  arr[i]=output[i];
+   // }
+   
+   return output;
+    
+   */     
         
        
         
