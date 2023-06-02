85391ddb4aaa06f4eb5815eabf5ee9a0a07d4543
Abrar Eyasir <97697364+eyasir2047@users.noreply.github.com>
2023-06-03 00:55:18 +0600

    Added solution - LeetHub

--- Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.cpp
+++ Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.cpp
@@ -36,35 +36,68 @@ int main(){
 
 // vec : given vector of elements
 // K : given value whose index we need to find 
-int Search(vector<int> arr, int K) {
-    //code here
+
+
+//User function template for C++
+
+// vec : given vector of elements
+// K : given value whose index we need to find 
+
+int getpivot(vector<int> arr, int n){
     
-    int n=arr.size();
+   
+    int left=0, right=n-1;
     
-    int left=0,right=n-1;
+    //if the array is sorted but not rotated
+    if(arr[left]<=arr[right]) return -1;
     
-    while(left<=right){
-        int mid=(left+right)/2;
-        if(arr[mid]==K)return mid;
-        else if(arr[mid]>=arr[left]){
-            if(K>=arr[left] && K<=arr[mid]){
-                right=mid-1;
-            }
-            else {
-                left=mid+1;
-            }
+    int mid=left+(right-left)/2;
+    
+    while(right>left){
+        if(arr[mid]>=arr[0]){
+            left=mid+1;
         }
-        else{ // arr[mid]<arr[left]
-            if(K>=arr[mid] && K<=arr[right]){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
+        else{//arr[n-1]>=arr[mid]
+        
+        right=mid;
+            
         }
+        
+        mid=left+(right-left)/2;
+        
     }
     
-    return -1;
+    return right;
+}
+
+int binarysearch(vector<int> arr,int left, int right,int x ){
+     int mid=left+(right-left)/2;
+     while(left<=right){
+         if(arr[mid]==x) return mid;
+         
+         else if(arr[mid]>x) {
+            right=mid-1;
+         }
+         else{
+             left=mid+1;
+         }
+         mid=left+(right-left)/2;
+     }
+     
+     return -1;
+}
+int Search(vector<int> vec, int K) {
+    //code here
+    int n=vec.size();
+    
+    int pivot=getpivot(vec,n);
+    
+    if(K<=vec[n-1] && K>=vec[pivot]){
+        return binarysearch(vec,pivot,n-1,K);
+    }
+    else{
+        return binarysearch(vec,0,pivot-1,K);
+    }
     
     
 }
