32166e282557538e8b64cb16cfb150c75d235d9b
Abrar Eyasir <97697364+eyasir2047@users.noreply.github.com>
2023-07-07 01:32:32 +0600

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,29 +10,34 @@ using namespace std;
 class Solution{   
 public:
 
-bool func(vector<int>&arr,int n,int sum,vector<vector<int>>&dp){
-    if(sum==0)return true;
-    //sum==0 && n==0-> return true;
-    if(n==0)return false;
-    
-    if(dp[n][sum]!=-1){
-        
-        return dp[n][sum]==1?true:false;
-    }
-    
-    if(arr[n-1]<=sum)
-    return dp[n][sum]=func(arr,n-1,sum-arr[n-1],dp)|| func(arr,n-1,sum,dp);
-    else
-    return dp[n][sum]=func(arr,n-1,sum,dp);
-}
 
     bool isSubsetSum(vector<int>arr, int sum){
      
-      int n=arr.size();
-      
-      vector<vector<int>>dp(n+10,vector<int>(sum+10,-1));
-      bool ans=func(arr,n,sum,dp);
-      return ans;
+    int n=arr.size();
+    bool dp[n+1][sum+1];
+    //i->arr[] andn j->sum
+    //[dp](n+1)*(sum+1)
+    
+    for(int i=0;i<=n;i++){
+        dp[i][0]=true;
+    }
+    for(int j=1;j<=sum;j++){
+        dp[0][j]=false;
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(arr[i-1]<=j){
+                dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][sum];
+        
+    
         
     }
 };
